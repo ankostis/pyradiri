a5383febd55920f9e72f73a305041add2b85ac9f
Kostis Anagnostopoulos <ankostis@gmail.com>
2017-03-22 13:53:46 +0100

    refact(main): move code from main into package
    + pep-366 for rel imports (not really needed).

--- pyradiri/__main__.py
+++ pyradiri/__main__.py
@@ -6,41 +6,16 @@
 # You may not use this work except in compliance with the Licence.
 # You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl
 
+
 import sys
 
-import jsonschema
-import pkg_resources
 
-from ruamel import yaml  # @UnresolvedImport
-
-
-def parse_yaml(finp, drop_comments=False):
-    return yaml.load(finp, yaml.Loader if drop_comments else yaml.RoundTripLoader)
-
-
-def validate_yaml_files(*files):
-    with pkg_resources.resource_stream(__name__, 'schema.yaml') as finp:  # @UndefinedVariable
-        schema_dict = parse_yaml(finp, True)
-    schema = jsonschema.Draft4Validator(schema_dict)
-
-    for f in files:
-        if f == '-':
-            txt = parse_yaml(sys.stdin)
-        else:
-            with open(f, 'rt') as fp:
-                txt = parse_yaml(fp)
-
-        if not txt:
-            raise ValueError('Empty file!')
-
-        err_header = '#' * 72
-        print(('\n\n%s\n' % err_header).join(str(s) for s in schema.iter_errors(txt)))
+__package__ = 'pyradiri'  # @ReservedAssignment pep-366
 
 
 def main(args=sys.argv):
+    from . import validate_yaml_files
     files = args[1:]
-    if not files:
-        files = ('-', )
 
     validate_yaml_files(*files)
 
